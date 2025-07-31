6d5cb52010d7a18dcedcd380f2804e0112d903b0
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-07-30 20:31:08 -0500

    Update and rename maxArea to maxArea.py

---
+++ maxArea.py
@@ -0,0 +1,19 @@
+
+# 11. Container With Most Water
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left = 0
+        right = len(height) - 1
+        res = 0
+        while  left < right:
+            if height[left] < height[right]:
+                res = max(res, height[left] * (right - left))
+                left += 1
+            else:
+                res = max(res, height[right] * (right - left))
+                right -= 1
+        return res
